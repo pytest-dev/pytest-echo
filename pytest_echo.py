4d67a794a9c0334caac8041a94b8542ea87305d8
sax <s.apostolico@gmail.com>
2014-08-05 10:36:07 +0200

    add ability to print generic attributes (--echo-attr))

--- pytest_echo.py
+++ pytest_echo.py
@@ -6,6 +6,87 @@ from pprint import pformat
 __version__ = '1.0'
 
 
+class RetrieveException(Exception):
+    pass
+
+
+def get_attr(obj, attr, default='NOT FOUND'):
+    """Recursive get object's attribute. May use dot notation.
+
+    >>> class C(object): pass
+    >>> a = C()
+    >>> a.b = C()
+    >>> a.b.c = 4
+    >>> get_attr(a, 'b.c')
+    4
+
+    >>> get_attr(a, 'b.c.y', None)
+
+    >>> get_attr(a, 'b.c.y', 1)
+    1
+    >>> get_attr([0,1,2], '2')
+    2
+    >>> get_attr([0,1,(21, 22)], '2.1')
+    22
+    >>> get_attr({'key': 11}, 'key')
+    11
+    >>> get_attr({'key': {'key': 11}}, 'key.key')
+    11
+    """
+
+    if '.' not in attr:
+        try:
+            if hasattr(obj, attr):
+                return getattr(obj, attr, default)
+            elif isinstance(obj, (list, tuple, set)):
+                return obj[int(attr)]
+            elif isinstance(obj, dict):
+                return obj[attr]
+            else:
+                return default
+        except Exception as e:
+            return str(e)
+    else:
+        L = attr.split('.')
+        return get_attr(get_attr(obj, L[0], default), '.'.join(L[1:]), default)
+
+
+def get_module_attribute(path):
+    """
+    Returns a attribute value base on it's full path.
+    The `attribute` can be either a module attribute (ie. os.path.curdir)
+    or a object attribute (ie. linecache.cache.__class__)
+
+    Warning: Be careful when use thi function as it load any module in the path
+    and this will execute any module's level code
+
+    :param path: full path to the attribute
+    :return:
+
+    >>> print get_module_attribute('linecache.cache.__class__')
+    <type 'dict'>
+    >>> print get_module_attribute('os.path.curdir')
+    '.'
+    """
+    parts = path.split('.')
+    parent = ""
+    pkg = None
+    try:
+        for i, el in enumerate(parts):
+            try:
+                if parent:
+                    a = "{}.{}".format(parent, parts[i])
+                else:
+                    a = parts[i]
+                pkg = __import__(a, fromlist=[parent])
+                parent = a
+            except ImportError:
+                if hasattr(pkg, el):
+                    return pformat(get_attr(pkg, ".".join(parts[i:])))
+    except Exception as e:
+        return str(e)
+
+
 def _get_version(package_name):
     try:
         pkg = __import__(package_name)
@@ -20,30 +101,11 @@ def _get_version(package_name):
                 return attr
 
 
-def _get_settings(entry):
-    try:
-        from django.core.exceptions import ImproperlyConfigured
-
-        try:
-            from django.conf import settings
-
-            getattr(settings, 'DEBUG')  # force settings loading
-        except ImproperlyConfigured:
-            settings.configure()
-        try:
-            entry = getattr(settings, entry)
-            return pformat(entry)
-        except AttributeError as e:
-            return "ERROR: %s" % str(e)
-    except ImportError:
-        return ""
-
-
 def pytest_report_header(config):
     ret = []
-    if config.option.echo_settings:
-        ret.append("\n".join(["%s: %s" % (k, _get_settings(k))
-                              for k in config.option.echo_settings]))
+    if config.option.echo_attribues:
+        ret.append("\n".join(["%s: %s" % (k, get_module_attribute(k))
+                              for k in config.option.echo_attribues]))
     if config.option.echo_envs:
         ret.append("\n".join(["%s: %s" % (k, os.environ.get(k, "<not set>"))
                               for k in config.option.echo_envs]))
@@ -60,5 +122,5 @@ def pytest_addoption(parser):
                     default=[], help="environment to print")
     group.addoption('--echo-version', action='append', dest="echo_versions",
                     default=[], help="package version to print")
-    group.addoption('--echo-settings', action='append', dest="echo_settings",
-                    default=[], help="django settings to print")
+    group.addoption('--echo-attr', action='append', dest="echo_attribues",
+                    default=[], help="attribute to print (full path)")
--- test_echo.py
+++ test_echo.py
@@ -3,17 +3,26 @@ import pytest
 import pytest_echo
 
 pytest_plugins = "pytester",
-try:
-    import django
 
-    django_present = True
-except ImportError:
-    django_present = False
+ATTR_INT = 111
+ATTR_DICT = {'key': 'value'}
+ATTR_LIST = [11, 12, 13, (21, 22)]
+ATTR_COMPOSITE = {'key1': 'value1', 'key2': [11, 12, 13, 14], 'key3': 99}
+
+
+class Dummy():
+    attr = 1
+
+
+def FUNC():
+    pass
+
+
+dummy = Dummy()
 
 
 def test_version():
     import pytest_echo
-
     assert pytest_echo.__version__
 
 
@@ -30,13 +39,68 @@ def test_echo_version(testdir):
 
 def test_echo_all(testdir):
     os.environ['PYTESTECHO'] = '123'
-    result = testdir.runpytest('--echo-version=pytest_echo', '--echo-env=PYTESTECHO')
+    result = testdir.runpytest('--echo-version=pytest_echo',
+                               '--echo-env=PYTESTECHO')
     assert "PYTESTECHO: 123" in result.stdout.lines
     assert "pytest_echo: %s" % pytest_echo.__version__ in result.stdout.lines
 
 
-@pytest.mark.skipif('not django_present')
-def test_echo_settings(testdir):
-    result = testdir.runpytest('--echo-settings=DEBUG')
-    assert 'DEBUG: False' in result.stdout.lines
+def test_echo_attr(testdir):
+    result = testdir.runpytest('--echo-attr=test_echo.ATTR_INT')
+    assert 'test_echo.ATTR_INT: 111' in result.stdout.lines
 
+
+def test_echo_attr_dict(testdir):
+    result = testdir.runpytest('--echo-attr=test_echo.ATTR_DICT.key')
+    assert u"test_echo.ATTR_DICT.key: 'value'" in result.stdout.lines
+
+
+def test_echo_attr_list(testdir):
+    result = testdir.runpytest('--echo-attr=test_echo.ATTR_LIST.2')
+    assert u"test_echo.ATTR_LIST.2: 13" in result.stdout.lines
+
+
+def test_echo_attr_list_inner(testdir):
+    result = testdir.runpytest('--echo-attr=test_echo.ATTR_LIST.3.1')
+    assert u"test_echo.ATTR_LIST.3.1: 22" in result.stdout.lines
+
+
+def test_echo_attr_list_composite(testdir):
+    result = testdir.runpytest('--echo-attr=test_echo.ATTR_COMPOSITE.key1',
+                               '--echo-attr=test_echo.ATTR_COMPOSITE.key2.3')
+    assert u"test_echo.ATTR_COMPOSITE.key1: 'value1'" in result.stdout.lines
+    assert u"test_echo.ATTR_COMPOSITE.key2.3: 14" in result.stdout.lines
+
+
+def test_echo_attr_list_callable(testdir):
+    result = testdir.runpytest('--echo-attr=test_echo.FUNC')
+    result.stdout.fnmatch_lines([
+        "test_echo.FUNC: <function FUNC*",
+    ])
+
+
+def test_echo_attr_object_attr(testdir):
+    result = testdir.runpytest('--echo-attr=test_echo.dummy.attr')
+    result.stdout.fnmatch_lines([
+        "test_echo.dummy.attr: 1",
+    ])
+
+
+def test_echo_attr_module_object_attr(testdir):
+    result = testdir.runpytest('--echo-attr=linecache.cache.__class__')
+    result.stdout.fnmatch_lines([
+        "linecache.cache.__class__: <type 'dict'>",
+    ])
+
+
+def test_django_settings(testdir):
+    p = testdir.makeconftest("""
+        def pytest_configure(config):
+            import django
+            from django.conf import settings  # noqa
+            settings.configure()
+    """)
+    result = testdir.runpytest('--echo-attr=django.conf.settings.DEBUG')
+    result.stdout.fnmatch_lines([
+        "django.conf.settings.DEBUG: False",
+    ])
